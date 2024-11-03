a6f34cf32350a5982d454567c6f8e9d9a4794227
Kbasius <kbasius@gmail.com>
2024-11-03 14:51:29 -0500

    Header+Navigation
    Added the Header component.
    
    Added the Navigation component.

---
+++ 02-Challenge/portfolio12/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Enter clever Title here.</h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
