6f5d699c4d23dd9de134ecfeeb5f2d50a3631992
Kbasius <kbasius@gmail.com>
2024-11-04 21:57:29 -0500

    Header CSS Stuff
    Attempting to do the CSS Stuff for Header.
    
    Got Header to were it actually belonged.
    Got links were they belonged.

--- 02-Challenge/portfolio12/src/components/Navigation.jsx
+++ 02-Challenge/portfolio12/src/components/Navigation.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../Navigation.css';
 
 const Navigation = () => {
   return (
     <nav>
-      <ul>
+      <ul className="nav-list">
         <li><Link to="/">About Me</Link></li>
         <li><Link to="/Project">Project</Link></li>
         <li><Link to="/contact">Contact</Link></li>
