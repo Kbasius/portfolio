e74a9cc47f19d9e915fe6b25f58cb1a130b9b9e9
Kbasius <kbasius@gmail.com>
2024-11-05 21:07:20 -0500

    CSS
    Added CSS for most of the .JSX components.
    
    Header and paragraph alignments, font ect.

--- 02-Challenge/portfolio12/src/components/AboutMe.jsx
+++ 02-Challenge/portfolio12/src/components/AboutMe.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import Navigation from './Navigation';
+import '../AboutMe.css';
 
 const AboutMe = () => {
     return (
-        <div>
-            <h1>About Me</h1>  
-            <p>I Will figure something out later.</p>
-            <Navigation/>
+        <div className="about-me-container about-me">
+            <div className="about-me-content">
+                <h1 className="header1">About Me</h1>  
+                <p>Not much is happening here, this is my cat and here is a decent roasted chicken recipe. Gather all ingredients. Preheat the oven to 350 degrees F.</p>
+                <p>Place chicken in a roasting pan; season generously inside and out with onion powder, salt, and pepper. Place 3 tablespoons of butter in chicken.</p>
+                <p>Arrange dollops of remaining butter on the outside of chicken.</p> 
+                <p>Cut celery into 3 or 4 pieces; place in the chicken cavity.</p>
+                <p>Bake chicken uncovered in the preheated oven until no longer pink at the bone and the juices run clear, about 1 hour and 15 minutes.</p>
+                <p>An instant-read thermometer inserted into the thickest part of the thigh, near the bone, should read 165 degrees F.</p>
+                <p>Remove from the oven and baste with drippings. Cover with aluminum foil and allow to rest for about 30 minutes before serving.</p>
+                <img src="./src/images/BeepandI.jpg" alt="My cat and I" className="about-me-image" />
+            </div>    
         </div>
     );
 };
--- 02-Challenge/portfolio12/src/components/Project.jsx
+++ 02-Challenge/portfolio12/src/components/Project.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Navigation from './Navigation';
+import '../Project.css';
 
 const Project = ({ title, description }) => {
     return (
         <div className="project">
             <h2>{title}</h2>
             <p>{description}</p>
-            <Navigation/>
         </div>
     );
 };
